30bdcb8b9b79611055f3049845025c1b94a8e282
BreadRoll-code <73705086+BreadRoll-code@users.noreply.github.com>
2020-11-10 21:05:28 +0100

    Added more labels to GUI

--- src/addSwimmer.java
+++ src/addSwimmer.java
@@ -8,28 +8,52 @@ public class addSwimmer extends Swimmer {
         JFrame box = new JFrame("frame");
         box.setBounds(150,100,1000,500);
 
-        JLabel Label1 = new JLabel("LABEL");
-        Label1.setBounds(500,30,50,100);
+        //Creation of Labels
+        JLabel Label1 = new JLabel("New Swimmer Creation");
+        Label1.setBounds(450,30,200,100);
 
+        JLabel Label2 = new JLabel("First name");
+        Label2.setBounds(50,110,75,100);
+
+        JLabel Label3 = new JLabel("Last name");
+        Label3.setBounds(50,160,75,100);
+
+        JLabel Label4 = new JLabel("Age");
+        Label4.setBounds(50,210,75,100);
+
+        JLabel Label5 = new JLabel("Group");
+        Label5.setBounds(50,260,75,100);
+
+        JLabel Label6 = new JLabel("Gender");
+        Label6.setBounds(50,310,75,100);
+
+        //Creation of Text Fields
         JTextField NameText1 = new JTextField();
-        NameText1.setBounds(100, 150, 100, 25);
+        NameText1.setBounds(125, 150, 100, 25);
 
         JTextField NameText2 = new JTextField();
-        NameText2.setBounds(100, 200, 100, 25);
+        NameText2.setBounds(125, 200, 100, 25);
 
         JTextField AgeText = new JTextField();
-        AgeText.setBounds(100, 250, 100, 25);
+        AgeText.setBounds(125, 250, 100, 25);
 
         JTextField GroupText = new JTextField();
-        GroupText.setBounds(100, 300, 100, 25);
+        GroupText.setBounds(125, 300, 100, 25);
 
         JTextField GenderText = new JTextField();
-        GenderText.setBounds(100, 350, 100, 25);
+        GenderText.setBounds(125, 350, 100, 25);
 
-        JButton Button1 = new JButton("Add Name");
-        Button1.setBounds(100,100,150,50);
+        //Creation of Button
+        JButton Button1 = new JButton("Create Swimmer");
+        Button1.setBounds(800,350,150,50);
 
+        //Adding to Box
         box.add(Label1);
+        box.add(Label2);
+        box.add(Label3);
+        box.add(Label4);
+        box.add(Label5);
+        box.add(Label6);
         box.add(Button1);
         box.add(NameText1);
         box.add(NameText2);
