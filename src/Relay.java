2619822acd5b34264fbd8da1ac56202d97a8d3dd
tther <ttherealanton@gmail.com>
2020-12-01 13:38:20 +0100

    made variables

--- src/Relay.java
+++ src/Relay.java
@@ -1,9 +1,20 @@
 public class Relay {
     public String Swimmer1;
     public String Swimmer2;
+    public String Swimmer3;
+    public String Swimmer4;
+    public Integer SwimmerAge1;
+    public Integer SwimmerAge2;
+    public Integer SwimmerAge3;
+    public Integer SwimmerAge4;
+    public Integer SwimmerGender1;
+    public Integer SwimmerGender2;
+    public Integer SwimmerGender3;
+    public Integer SwimmerGender4;
+    public Integer SwimmerPrimary;
+    public Integer RaceChosen;
 
-
-
+    //Sets variables
     public void setSwimmer1(String swimmer1) {
         Swimmer1 = swimmer1;
     }
@@ -12,11 +23,56 @@ public class Relay {
         Swimmer2 = swimmer2;
     }
 
-    public String getSwimmer1() {
-        return Swimmer1;
-    }
+    public void setSwimmer3(String swimmer3) { Swimmer3 = swimmer3; }
 
-    public String getSwimmer2() {
-        return Swimmer2;
-    }
+    public void setSwimmer4(String swimmer4) { Swimmer4 = swimmer4; }
+
+    public void setSwimmerAge1(Integer swimmerAge1) { SwimmerAge1 = swimmerAge1; }
+
+    public void setSwimmerAge2(Integer swimmerAge2) { SwimmerAge2 = swimmerAge2; }
+
+    public void setSwimmerAge3(Integer swimmerAge3) { SwimmerAge3 = swimmerAge3; }
+
+    public void setSwimmerAge4(Integer swimmerAge4) { SwimmerAge4 = swimmerAge4; }
+
+    public void setSwimmerGender1(Integer swimmerGender1) { SwimmerGender1 = swimmerGender1; }
+
+    public void setSwimmerGender2(Integer swimmerGender2) { SwimmerGender2 = swimmerGender2; }
+
+    public void setSwimmerGender3(Integer swimmerGender3) { SwimmerGender3 = swimmerGender3; }
+
+    public void setSwimmerGender4(Integer swimmerGender4) { SwimmerGender4 = swimmerGender4; }
+
+    public void setSwimmerPrimary(Integer swimmerPrimary) { SwimmerPrimary = swimmerPrimary; }
+
+    public void setRaceChosen(Integer raceChosen) { RaceChosen = raceChosen; }
+
+    //Outputs variables
+    public String getSwimmer1() { return Swimmer1; }
+
+    public String getSwimmer2() { return Swimmer2; }
+
+    public String getSwimmer3() { return Swimmer3; }
+
+    public String getSwimmer4() { return Swimmer4; }
+
+    public Integer getSwimmerAge1() { return SwimmerAge1; }
+
+    public Integer getSwimmerAge2() { return SwimmerAge2; }
+
+    public Integer getSwimmerAge3() { return SwimmerAge3; }
+
+    public Integer getSwimmerAge4() { return SwimmerAge4; }
+
+    public Integer getSwimmerGender1() { return SwimmerGender1; }
+
+    public Integer getSwimmerGender2() { return SwimmerGender2; }
+
+    public Integer getSwimmerGender3() { return SwimmerGender3; }
+
+    public Integer getSwimmerGender4() { return SwimmerGender4; }
+
+    public Integer getSwimmerPrimary() { return SwimmerPrimary; }
+
+    public Integer getRaceChosen() { return RaceChosen; }
 }
